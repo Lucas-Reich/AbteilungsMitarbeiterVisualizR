6ea7f102f29f48615b259a7e5e6ed91be6b9262b
Lucas Reich <lucas.reich@flixbus.com>
2018-10-23 11:09:23 +0200

    started fachkonzept test

--- src/AbteilungsMitarbeiterVisualizR/Fachkonzept/FachkonzeptTest.java
+++ src/AbteilungsMitarbeiterVisualizR/Fachkonzept/FachkonzeptTest.java
@@ -1,5 +1,61 @@
-import static org.junit.jupiter.api.Assertions.*;
+package AbteilungsMitarbeiterVisualizR.Fachkonzept;
+
+
+import AbteilungsMitarbeiterVisualizR.Persistence.IPersistence;
+import org.junit.Assert;
+import org.junit.Test;
+import org.mockito.Mock;
 
 class FachkonzeptTest {
 
+    @Test
+    void getProjectName(Fachkonzept fk) {
+
+        Assert.assertEquals("AbteilungsMitarbeiterVisualizR", fk.getProjectName());
+    }
+
+    @Test
+    void getDepartment() {
+//        Fachkonzept fachkonzept = new Fachkonzept1(persistence);
+    }
+
+    @Test
+    void getDepartments() {
+    }
+
+    @Test
+    void saveDepartment() {
+    }
+
+    @Test
+    void deleteDepartment() {
+    }
+
+    @Test
+    void updateDepartmentName() {
+    }
+
+    @Test
+    void getEmployee() {
+    }
+
+    @Test
+    void getEmployees() {
+    }
+
+    @Test
+    void saveNewEmployee() {
+    }
+
+    @Test
+    void deleteEmployee() {
+    }
+
+    @Test
+    void updateEmployeeName() {
+    }
+
+    @Test
+    void reassingEmployeeDepartment() {
+    }
 }
