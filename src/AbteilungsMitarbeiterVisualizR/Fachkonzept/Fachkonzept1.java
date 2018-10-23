ccf1685fec58ba2b431862775c2e7498f1931c47
Lucas Reich <lucas.reich@flixbus.com>
2018-10-23 11:04:46 +0200

    implement fachkonzept1

--- src/AbteilungsMitarbeiterVisualizR/Fachkonzept/Fachkonzept1.java
+++ src/AbteilungsMitarbeiterVisualizR/Fachkonzept/Fachkonzept1.java
@@ -4,9 +4,14 @@ import AbteilungsMitarbeiterVisualizR.Entities.Department;
 import AbteilungsMitarbeiterVisualizR.Entities.Employee;
 import AbteilungsMitarbeiterVisualizR.Persistence.IPersistence;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Fachkonzept1 implements Fachkonzept {
+    private static final String PROJECT_NAME = "AbteilungsMitarbeiterVisualizR";
+
     private IPersistence mPersistence;
     // TODO Fachkonzept1 gibt die Daten alphabetisch (A-Z) an
 
@@ -15,63 +20,98 @@ public class Fachkonzept1 implements Fachkonzept {
     public Fachkonzept1(IPersistence persistence) {
         mPersistence = persistence;
     }
+
     @Override
     public String getProjectName() {
-        return null;
+        return PROJECT_NAME;
     }
 
     @Override
     public Department getDepartment(long departmentId) {
-        return null;
+        return mPersistence.getDepartment(departmentId);
     }
 
     @Override
     public List<Department> getDepartments() {
-        return null;
+        List<Department> departments = mPersistence.getAllDepartments();
+
+        departments.sort(new Comparator<Department>() {
+            @Override
+            public int compare(Department o1, Department o2) {
+                return Character.compare(o1.getName().charAt(0), o2.getName().charAt(0));
+            }
+        });
+
+
+        return departments;
     }
 
     @Override
     public void saveDepartment(String departmentName) {
+        Department department = new Department(departmentName);
 
+        mPersistence.saveDepartment(department);
     }
 
     @Override
     public void deleteDepartment(long departmentId) {
-
+        mPersistence.deleteDepartment(departmentId);
     }
 
     @Override
     public void updateDepartmentName(String name, long departmentId) {
+        Department updatedDepartment = new Department(
+                departmentId,
+                name
+        );
 
+        mPersistence.updateDepartment(updatedDepartment);
     }
 
     @Override
     public Employee getEmployee(long employeeId) {
-        return null;
+        return mPersistence.getEmployee(employeeId);
     }
 
     @Override
     public List<Employee> getEmployees(long departmentId) {
-        return null;
+        List<Employee> employees = mPersistence.getAllEmployees();
+
+        employees.sort(new Comparator<Employee>() {
+            @Override
+            public int compare(Employee o1, Employee o2) {
+                return Character.compare(o1.getName().charAt(0), o2.getName().charAt(0));
+            }
+        });
+
+        return employees;
     }
 
     @Override
     public void saveNewEmployee(String employeeName, long departmentId) {
+        Employee employee = new Employee(employeeName);
 
+        mPersistence.saveEmployee(employee, departmentId);
     }
 
     @Override
     public void deleteEmployee(long employeeId) {
 
+        mPersistence.deleteEmployee(employeeId);
     }
 
     @Override
     public void updateEmployeeName(String name, long employeeId) {
+        Employee updatedEmployee = new Employee(employeeId, name);
 
+        mPersistence.updateEmployee(updatedEmployee);
     }
 
     @Override
     public void reassingEmployeeDepartment(long employeeId, long departmentId) {
+        Employee employee = mPersistence.getEmployee(employeeId);
 
+        mPersistence.deleteEmployee(employeeId);
+        mPersistence.saveEmployee(employee, departmentId);
     }
 }
